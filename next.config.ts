f9f7b6bf25a624e0c5457f1fa87045ec5bd46fd3
quynhanh777 <tuananhqhvl777@gmail.com>
2026-02-05 17:42:00 +0700

    deploy_fix_css

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
   images: {
     unoptimized: true,
   },
+  // Đảm bảo đường dẫn này khớp chính xác với URL của bạn
+  basePath: "/Hinh-Anh-Cua-THIEN-CHUA-Trong-Sach-GIOP",
+  assetPrefix: "/Hinh-Anh-Cua-THIEN-CHUA-Trong-Sach-GIOP",
 };
 
 export default nextConfig;
